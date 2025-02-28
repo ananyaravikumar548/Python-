18d1b97cc1841806c1902dca96a282567c105cd5
ananyaravikumar548 <ananyaravikumar71@gmail.com>
2025-02-28 10:57:53 +0530

    Create program10.py

---
+++ lab5/program10.py
@@ -0,0 +1,7 @@
+import numpy as npy
+arr_1 = npy.array([[1., 2., 3.], [4., 5., 6.]])
+arr_2 = npy.array([[7., 8., 9.], [1., 3., 5.]])
+print("Original array:\n", arr_1, arr_2)
+print("Element-wise addition:\n", arr_1 + arr_2)
+print("Element-wise subtraction:\n", arr_1 - arr_2)
+print("Element-wise multiplication:\n", arr_1 * arr_2) # np.
