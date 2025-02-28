b1441932212fbb08d592c43c50e39b436f2f5230
ananyaravikumar548 <ananyaravikumar71@gmail.com>
2025-02-28 10:03:58 +0530

    Create program2.py

---
+++ lab5/program2.py
@@ -0,0 +1,12 @@
+import numpy as np
+data1=[1,2,3,4]
+arr1=np.array(data1)
+print (arr1)
+A=arr1.shape
+print(A)
+print(arr1.dtype)
+print(arr1.size)
+print(type(arr1))
+print(arr1.ndim)
+print(arr1.itemsize)
+print(arr1.size*arr1.itemsize)
