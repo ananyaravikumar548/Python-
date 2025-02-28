dd705672775528472c3eafac8f8ae4f8960df793
ananyaravikumar548 <ananyaravikumar71@gmail.com>
2025-02-28 10:39:49 +0530

    Create program7.py

---
+++ lab5/program7.py
@@ -0,0 +1,4 @@
+arr_3 = np.array([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.], [1., 3., 5.]])
+arr_tranp = arr_3.transpose()
+print("Original matrix:\n", arr_3)
+print("Transpose matrix:\n", arr_tranp)
