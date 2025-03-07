9407ac489a1312a5bf2a946e74959eb7131d2db4
ananyaravikumar548 <ananyaravikumar71@gmail.com>
2025-03-07 20:25:12 +0530

    Create program3.py

---
+++ lab6/program3.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+population_dict = {
+"California": 38332521,
+"Texas": 26448193,
+"Florida": 19552860,
+"Illinois": 12882135,
+}
+population = pd.Series(population_dict)
+print(population_dict, type(population_dict))
+print(population, type(population))
